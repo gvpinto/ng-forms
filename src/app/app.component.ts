be1d4774e9c41a7cee422409f763e026508a34d2
Glenn Pinto <gvpinto@gmail.com>
2021-12-27 20:25:49 -0500

    188. TD: Submitting and Using the Form - Code 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  onSubmit(form: HTMLFormElement) {
+    console.log(form);
   }
 }
