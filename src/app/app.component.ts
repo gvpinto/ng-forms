9cb9412a179ccf9856d692ad3b9dbad838954456
Glenn Pinto <gvpinto@gmail.com>
2021-12-28 16:22:04 -0500

    199. Setting and Patching Form Values - setValue()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,15 @@ export class AppComponent {
   genders = ['male', 'female'];
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.signupForm.setValue({
+      userData: {
+        username: suggestedName,
+        email: '',
+      },
+      secret: 'teacher',
+      questionAnswer: '',
+      gender: 'male',
+    });
   }
 
   //   onSubmit(form: NgForm) {
